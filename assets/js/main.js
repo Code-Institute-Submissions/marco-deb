e6ef6ed2941dfbca37b6509c51c960e8f699416b
Marc-Solution <marco.deb@gmail.com>
2021-01-01 19:32:49 +0000

    Add Dark/light background option to privacy-policy.html

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+//  I got this code from  Code instutute’s basic Javascript Setting Properties - Part 2
+ function lightTheme() {
+    el = document.getElementById("mainDiv");
+    el.classList.remove("dark");
+    el.classList.add("light");
+}
+
+function darkTheme() {
+    el = document.getElementById("mainDiv");
+    el.classList.remove("light");
+    el.classList.add("dark");
+}
+
+function bodySection() {
+    el = document.getElementById("body-section")
+    el.classList.add("remove-body-section")
+}
