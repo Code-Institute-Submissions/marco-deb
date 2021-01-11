7d181c6adc7cf65cac769064299963840d01294f
Marc-Solution <marco.deb@gmail.com>
2021-01-11 10:26:07 +0000

    Testing contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,3 +38,20 @@ function slideToggleElement(elementToShow, elementToHide) {
     $(elementToHide).slideUp();
     $(elementToShow).slideDown();
 }
+//------------------------------------
+
+function sendMail(contactForm) {
+    emailjs.send('gmail', 'rosie', {
+        'from_name': contactForm.name.value, 
+        'from_email': contactForm.emailaddress.value,
+        'project_request': contactForm.projectsummary.value
+    })
+    .then(
+        function(response){
+            console.log('success', response);
+        },
+        function(error) { 
+            console.log('failed', error);
+        }); 
+        return false;  // To block from loading a new page
+}
