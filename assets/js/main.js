a33f0d93710da3be7c4bcd867754add00fde5145
Marc-Solution <marco.deb@gmail.com>
2021-01-11 11:23:38 +0000

    Removed download meditation section

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,18 +40,3 @@ function slideToggleElement(elementToShow, elementToHide) {
 }
 //------------------------------------
 
-function sendMail(contactForm) {
-    emailjs.send('gmail', 'rosie', {
-        'from_name': contactForm.name.value, 
-        'from_email': contactForm.emailaddress.value,
-        'project_request': contactForm.projectsummary.value
-    })
-    .then(
-        function(response){
-            console.log('success', response);
-        },
-        function(error) { 
-            console.log('failed', error);
-        }); 
-        return false;  // To block from loading a new page
-}
