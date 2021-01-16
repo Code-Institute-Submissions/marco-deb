2fbfdba82a23bafbfe000b2b81552de093826014
Marc-Solution <marco.deb@gmail.com>
2021-01-16 20:23:32 +0000

    Changed Contact form on contact.html

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -17,3 +17,21 @@ function sendMail(contactForm) {
 function downloadMessage() {
     alert("It's done. A download link has been sent to your email")
 }
+
+//---------------------------------------------
+
+function send(contactForm) {
+    emailjs.send('gmail', 'Marco-Deb', {
+        'from_name': contactForm.name.value, 
+        'from_email': contactForm.emailaddress.value,
+        // 'project_request': contactForm.projectsummary.value
+    })
+    .then(
+        function(response){
+            console.log('success', response);
+        },
+        function(error) { 
+            console.log('failed', error);
+        }); 
+        return false;  // To block from loading a new page
+}
