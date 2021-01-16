5ce3884d22a2acf68dc13113605874618bebea0c
Marc-Solution <marco.deb@gmail.com>
2021-01-16 10:38:03 +0000

    Add download meditation section

---
+++ assets/js/sendEmail.js.js
@@ -0,0 +1,20 @@
+//  I got this code from the emailJS lessons. 
+function sendMail(contactForm) {
+    emailjs.send('gmail', 'rosie', {
+        'from_name': contactForm.name.value, 
+        'from_email': contactForm.emailaddress.value,
+        // 'project_request': contactForm.projectsummary.value
+    })
+    .then(
+        function(response){
+            console.log('success', response);
+        },
+        function(error) { 
+            console.log('failed', error);
+        }); 
+        return false;  // To block from loading a new page
+}
+
+function downloadMessage() {
+    alert("It's done. A download link has been sent to your email")
+}
